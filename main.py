dd47cf30b618d8a6bd70aab79b99fb2b5ae81536
Nickolas <nickolaspiresc@gmail.com>
2026-03-15 00:24:53 -0300

    Data Cleaning

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 '''
-Organizar os dados, primeiro usar a formataçao de strings junto ao beautigul soup
 Categorias disnponiveis: business entertainment generalhealth science sports technology
 '''
+#title and url
 
 #Import modules
 import os
+import json
 import requests
 from dotenv import load_dotenv
 
 #Load API key
-api_key = os.getenv("API_KEY")
 load_dotenv()
+api_key = os.getenv("API_KEY")
 
 #Request
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
 response = requests.get(url)
 
+#Data JSON
+data = response.json()
+
 #Verification of status request and web scraping
+#The API that news in key "articles"
 if response.status_code == 200:
-    print("\nRequest successful\n")
+    articles = data.get('articles', [])
+    
+    for element in articles:
+        title = element.get('title')
+        url = element.get('url')
+        
+        print(f"Title: {title}\nLink: {url}\n{'-'*30}")
 else:
-    print("\nError 404\n")
+    print(f"\nError {response.status_code}\n")
